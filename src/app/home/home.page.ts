663aa6240cefc4b008088179276b744798ccfcb9
Fernando Mendes <marraia@MacBook-Air-de-Fernando.local>
2020-03-18 18:04:11 -0300

    Exemplo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var cordova: any;
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  executar() : void {
+    var success = function(result) {
+      alert("EXECUTOU NO CORDOVA"+ JSON.stringify(result, undefined, 2));
+    }
+    var failure = function(result) {
+      alert("NAO EXECUTOU" + JSON.stringify(result, undefined, 2));
+    }
+    cordova.plugins.HelloWorld.coolMethod({
+      _sMessage: "Hello World"
+    }, success, failure);
+  }
 
 }
